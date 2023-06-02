b16acc203fa7e9f5bd8f870ba260359fa028e145
Lee_Coderrr <80030418+Lee-Coderrr@users.noreply.github.com>
2023-06-02 18:34:20 +0900

    JDBC Programming code added

---
+++ src/main/java/org/example/ConnectionManager.java
@@ -0,0 +1,41 @@
+
+package org.example;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    public static final String ORG_H_2_DRIVER = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final int MAX_POOL_SIZE = 40;
+    private static final DataSource ds;
+
+    static {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(ORG_H_2_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername("sa");
+        hikariDataSource.setPassword("");
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+
+        ds = hikariDataSource;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
+    }
+}
